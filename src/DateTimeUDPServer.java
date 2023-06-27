0d616df1add7d8b6f1106a9a349d8533d9e2ebe7
depakmagr <magardeepak606@gmail.com>
2023-06-27 09:10:44 +0545

    UDP Server and Client

---
+++ src/DateTimeUDPServer.java
@@ -0,0 +1,36 @@
+import javax.xml.crypto.Data;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DateTimeUDPServer {
+    private final static int PORT = 13;
+    private final static Logger audit = Logger.getLogger("requests");
+    private final static Logger errors = Logger.getLogger("errors");
+    public static void main(String[] Args) {
+        try (DatagramSocket socket = new DatagramSocket(PORT)) {
+            while (true) {
+                try {
+                    DatagramPacket request = new DatagramPacket(new byte[1024], 1024);
+                    socket.receive(request);
+                    String daytime = new Date().toString();
+                    byte[] data = daytime.getBytes("US-ASCII");
+                    DatagramPacket response = new DatagramPacket(data, data.length,
+                            request.getAddress(), request.getPort());
+                    socket.send(response);
+                    audit.info(daytime + " " + request.getAddress());
+                } catch (IOException | RuntimeException ex) {
+                    errors.log(Level.SEVERE, ex.getMessage(), ex);
+                }
+            }
+        } catch (IOException ex) {
+            errors.log(Level.SEVERE, ex.getMessage(), ex);
+        }
+    }
+}
+
---
+++ src/DatetimeUDPClient.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class DatetimeUDPClient {
+    private final static int PORT = 13;
+    private static final String HOSTNAME = "localhost";
+    public static void main(String[] args) {
+        try (DatagramSocket socket = new DatagramSocket(0)) {
+            socket.setSoTimeout(10000);
+            InetAddress host = InetAddress.getByName(HOSTNAME);
+            DatagramPacket request = new DatagramPacket(new byte[1], 1, host, PORT);
+            DatagramPacket response = new DatagramPacket(new byte[1024], 1024);
+            socket.send(request);
+            socket.receive(response);
+            String result = new String(response.getData(), 0, response.getLength(),
+                    "US-ASCII");
+            System.out.println(result);
+        }catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
